68cc4135a6d780931806e3629347a2a6c60ee025
Christine Apondi Onyango <christine.onyango@ip-192-168-210-1.eu-west-1.compute.internal>
2021-11-13 11:25:33 +0300

    Generate random username and email

--- bb-interview-api/test/users.js
+++ bb-interview-api/test/users.js
@@ -1,122 +1,125 @@
-import supertest from 'supertest';
-const request = supertest('https://candidatex:qa-is-cool@qa-task.backbasecloud.com/api');
+import supertest from "supertest";
 
-import { expect } from 'chai';
+const request = supertest(
+  "https://candidatex:qa-is-cool@qa-task.backbasecloud.com/api"
+);
 
-const TOKEN= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOGNlOTMxZjcyZWQwMjcwNmFkNWI4MyIsInVzZXJuYW1lIjoiY2hyaXN0aW5lIiwiZXhwIjoxNjQxOTAwMTYzLCJpYXQiOjE2MzY3MTYxNjN9.x8PxPCyNIBiBGbUzRpCc-DJ19Aq2pm5QyU8C5rEAdug';
+import { expect } from "chai";
 
+let token = "";
+function generateString() {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  const length = 6;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
-describe('Users', () => {
-  it('Login', () => {
-      const data = {
-        user: {
-          email: 'tinaonyango@gmail.com',
-          password: 'Password1*'
-        }
-      };
-      return request
-        .post('/users/login')
-        .send(data)
-        .then((res) => {
-          //console.log(res.body);
-          expect(res.status).to.equal(200);
-          expect(res.body.user.username).to.be.eq('hau');
-          expect(res.body).to.include.keys('user');
-
-        });
-    });
-
-  it('Update User should be successful', () => {
+describe("Users", () => {
+  it("Login", () => {
     const data = {
-        user:{
-          email: 'tinaonyango@gmail.com',
-          bio: 'I like to skateboard',
-          image: 'https://i.stack.imgur.com/xHWG8.jpg'
-        }
-      };
+      user: {
+        email: "tinaonyango@gmail.com",
+        password: "Password1*",
+      },
+    };
+    return request
+      .post("/users/login")
+      .send(data)
+      .then((res) => {
+        token = res.body.user.token;
+        expect(res.status).to.equal(200);
+        console.log(res.body);
+
+        expect(res.body.user.username).to.be.eq("hau");
+        expect(res.body).to.include.keys("user");
+      });
+  });
+
+  it("Update User should be successful", () => {
+    const data = {
+      user: {
+        email: "tinaonyango@gmail.com",
+        bio: "I like to skateboard",
+        image: "https://i.stack.imgur.com/xHWG8.jpg",
+      },
+    };
 
     return request
-      .put('/user')
-      .set('jwtauthorization', 'Token ' + TOKEN)
+      .put("/user")
+      .set("jwtauthorization", "Token " + token)
       .send(data)
       .then((res) => {
         console.log(res.body);
         expect(res.status).to.equal(200);
-        expect(res.body.user.username).to.be.eq( 'hau');
-        expect(res.body).to.include.keys('user')
-        //console.log(res.body);
-
+        expect(res.body.user.username).to.be.eq("hau");
+        expect(res.body).to.include.keys("user");
       });
-      
   });
 
-  it('Updating a user should require authorization', () => {
+  it("Updating a user should require authorization", () => {
     const data = {
-        user:{
-          email: 'tinaonyango@gmail.com',
-          bio: 'I like to skateboard',
-          image: 'https://i.stack.imgur.com/xHWG8.jpg'
-        }
-      };
+      user: {
+        email: "tinaonyango@gmail.com",
+        bio: "I like to skateboard",
+        image: "https://i.stack.imgur.com/xHWG8.jpg",
+      },
+    };
 
     return request
-      .put('/user')
+      .put("/user")
       .send(data)
       .then((res) => {
-        //console.log(res.body);
         expect(res.status).to.equal(401);
-
-      });
-    });
-
-  it('Registration should be successful ', () => {
-    const data = {
-        user:{
-          username: 'hau99',
-          email: 'hallo@hallo.com',
-          password: 'Password1*'
-        }
-      };
-
-    return request
-      .post('/users')
-      .set('jwtauthorization', 'Token ' + TOKEN)
-      .send(data)
-      .then((res) => {
-      //console.log(res.body);
-        expect(res.status).to.equal(422);
-      });
-  });
-  it('Registration should validate email format', () => {
-    const data = {
-        user:{
-          username: 'hau99',
-          email: 'hau@hau',
-          password: 'Password1*'
-        }
-      };
-
-    return request
-      .post('/users')
-      .set('jwtauthorization', 'Token ' + TOKEN)
-      .send(data)
-      .then((res) => {
-        //console.log(res.body);
-        expect(res.status).to.equal(422);
       });
   });
 
-  it('Delete Articles', () => {
+  it("Registration should be successful ", () => {
+    const username = generateString();
+    const data = {
+      user: {
+        username,
+        email: `${username}@gmail.com`,
+        password: "Password1*",
+      },
+    };
+
     return request
-      .delete('DELETE articles/:bla-is-the-bla-of-bla-and-bla-w4yhpz')
-      .set('jwtauthorization', 'Token ' + TOKEN)
+      .post("/users")
+      .set("jwtauthorization", "Token " + token)
+      .send(data)
       .then((res) => {
-        //console.log(res.body);
+        expect(res.status).to.equal(200);
+      });
+  });
+  it("Registration should validate email format", () => {
+    const data = {
+      user: {
+        username: "hau99",
+        email: "hau@hau",
+        password: "Password1*",
+      },
+    };
+
+    return request
+      .post("/users")
+      .set("jwtauthorization", "Token " + token)
+      .send(data)
+      .then((res) => {
+        expect(res.status).to.equal(422);
+      });
+  });
+
+  it("Delete Articles", () => {
+    return request
+      .delete("DELETE articles/:bla-is-the-bla-of-bla-and-bla-w4yhpz")
+      .set("jwtauthorization", "Token " + token)
+      .then((res) => {
         expect(res.status).to.equal(404);
-       
       });
   });
 });
-
-
-//Pending. Reports, random email and string generator
--- bb-interview/Pages/login.js
+++ bb-interview/Pages/login.js
@@ -1,43 +1,36 @@
+let staticData = require("../Data/commonData.json");
+let loginPage = function () {
+  let Email = element.all(by.css('input[formcontrolname="email"]'));
+  let Password = element.all(by.css('input[formcontrolname="password"]'));
+  let submitButton = element.all(
+    by.className("btn btn-lg btn-primary pull-xs-right")
+  );
+  let signin = element(by.cssContainingText(".nav-link", " Sign in"));
+  let loginError = element(by.className("error-messages"));
+  let getProfile = element(by.cssContainingText(".nav-link", "christine"));
 
-let staticData = require('../Data/commonData.json')
-let loginPage=function(){
-
-    let Email=element.all(by.css('input[formcontrolname="email"]'));
-    let Password=element.all(by.css('input[formcontrolname="password"]'));
-    let submitButton=element.all(by.className('btn btn-lg btn-primary pull-xs-right'));
-    let signin=element(by.cssContainingText('.nav-link', ' Sign in'));
-    let loginError=element(by.className('error-messages'));
-    let getProfile=element(by.cssContainingText('.nav-link', 'christine'));
-
-
-    //baseURL
-    this.get=function(url)
-    {
+  //baseURL
+  this.get = function (url) {
     browser.get(url);
-    }
-    //signIn URL
-    this.signIn=function()
-    {
-        signin.click();
-    }
-    this.enterUsername=function(email)
-    {
-        Email.sendKeys(email);
-    }
-    this.enterPassword=function(password){
-        Password.sendKeys(password);
-    }
-    this.submit=function()
-    {
-        submitButton.click();
-    }
-    this.getError=function(error){
-        expect(loginError.getText()).toEqual('email or password is invalid');
-    }
-    this.Profile=function(profileName){
-        expect(getProfile.getText()).toEqual(staticData.Username);
-    }
-    
-    
+  };
+  //signIn URL
+  this.signIn = function () {
+    signin.click();
+  };
+  this.enterUsername = function (email) {
+    Email.sendKeys(email);
+  };
+  this.enterPassword = function (password) {
+    Password.sendKeys(password);
+  };
+  this.submit = function () {
+    submitButton.click();
+  };
+  this.getError = function (error) {
+    expect(loginError.getText()).toEqual("email or password is invalid");
+  };
+  this.Profile = function (profileName) {
+    expect(getProfile.getText()).toEqual(staticData.Username);
+  };
 };
-module.exports=new loginPage();
+module.exports = new loginPage();
